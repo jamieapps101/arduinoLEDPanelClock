83190807b290ebf91bce018e4b2fbfc8d3d90a54
jamie <jamieapps101@gmail.com>
2020-07-19 15:06:28 +0100

    setup env with new arduino cli setup

---
+++ Makefile
@@ -0,0 +1,78 @@
+PORT = "/dev/ttyUSB0"
+VERBOSE=true
+
+# to list all below:
+# $ ./arduino-cli board listall
+
+# BOARD_FQBN="esp8266:esp8266:gen4iod"          #- 4D Systems gen4 IoD Range       
+BOARD_FQBN="esp8266:esp8266:huzzah"           #- Adafruit Feather HUZZAH ESP8266 
+# BOARD_FQBN="esp8266:esp8266:wifi_slot"        #- Amperka WiFi Slot               
+# BOARD_FQBN="esp8266:esp8266:arduino"-esp8266  #- Arduino                         
+# BOARD_FQBN="esp8266:esp8266:espmxdevkit"      #- DOIT ESP-Mx DevKit (ESP8285)    
+# BOARD_FQBN="esp8266:esp8266:oak"              #- Digistump Oak                   
+# BOARD_FQBN="esp8266:esp8266:espduino"         #- ESPDuino (ESP-13 Module)        
+# BOARD_FQBN="esp8266:esp8266:espectro"         #- ESPectro Core                   
+# BOARD_FQBN="esp8266:esp8266:espino"           #- ESPino (ESP-12 Module)          
+# BOARD_FQBN="esp8266:esp8266:espresso_lite_v1" #- ESPresso Lite 1.0               
+# BOARD_FQBN="esp8266:esp8266:espresso_lite_v2" #- ESPresso Lite 2.0               
+# BOARD_FQBN="esp8266:esp8266:generic"          #- Generic ESP8266 Module          
+# BOARD_FQBN="esp8266:esp8266:esp8285"          #- Generic ESP8285 Module          
+# BOARD_FQBN="esp8266:esp8266:sonoff"           #- ITEAD Sonoff                    
+# BOARD_FQBN="esp8266:esp8266:inventone"        #- Invent One                      
+# BOARD_FQBN="esp8266:esp8266:d1_mini"          #- LOLIN(WEMOS) D1 R2 & mini       
+# BOARD_FQBN="esp8266:esp8266:d1_mini_lite"     #- LOLIN(WEMOS) D1 mini Lite       
+# BOARD_FQBN="esp8266:esp8266:d1_mini_pro"      #- LOLIN(WEMOS) D1 mini Pro        
+# BOARD_FQBN="esp8266:esp8266:nodemcu"          #- NodeMCU 0.9 (ESP-12 Module)     
+# BOARD_FQBN="esp8266:esp8266:nodemcuv2"        #- NodeMCU 1.0 (ESP-12E Module)    
+# BOARD_FQBN="esp8266:esp8266:modwifi"          #- Olimex MOD-WIFI-ESP8266(-DEV)   
+# BOARD_FQBN="esp8266:esp8266:phoenix_v1"       #- Phoenix 1.0                     
+# BOARD_FQBN="esp8266:esp8266:phoenix_v2"       #- Phoenix 2.0                     
+# BOARD_FQBN="esp8266:esp8266:eduinowifi"       #- Schirmilabs Eduino WiFi         
+# BOARD_FQBN="esp8266:esp8266:wiolink"          #- Seeed Wio Link                  
+# BOARD_FQBN="esp8266:esp8266:blynk"            #- SparkFun Blynk Board            
+# BOARD_FQBN="esp8266:esp8266:thing"            #- SparkFun ESP8266 Thing          
+# BOARD_FQBN="esp8266:esp8266:thingdev"         #- SparkFun ESP8266 Thing Dev      
+# BOARD_FQBN="esp8266:esp8266:esp210"           #- SweetPea ESP-210                
+# BOARD_FQBN="esp8266:esp8266:espinotee"        #- ThaiEasyElec's ESPino           
+# BOARD_FQBN="esp8266:esp8266:d1"               #- WeMos D1 R1                     
+# BOARD_FQBN="esp8266:esp8266:wifiduino"        #- WiFiduino                       
+# BOARD_FQBN="esp8266:esp8266:wifinfo"          #- WifInfo                         
+# BOARD_FQBN="esp8266:esp8266:cw01"             #- XinaBox CW01                    
+
+
+
+.PHONY: all compile verify setup
+
+default:
+	@echo "Options are:"
+	@echo "-  verify"
+	@echo "-  compile"
+
+
+
+arduino-cli:
+	wget https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz
+	tar -xf arduino-cli_latest_Linux_64bit.tar.gz
+	rm -rf arduino-cli_latest_Linux_64bit.tar.gz
+
+arduino-cli.yaml: arduino-cli
+	./arduino-cli config init \
+		--additional-urls \
+			https://dl.espressif.com/dl/package_esp32_index.json \
+		--additional-urls \
+			http://arduino.esp8266.com/stable/package_esp8266com_index.json \
+		--dest-dir .
+	./arduino-cli core update-index
+	./arduino-cli core install esp8266:esp8266
+	./arduino-cli board listall
+
+setup: arduino-cli.yaml
+
+compile: setup
+	./arduino-cli compile --fqbn $(BOARD_FQBN) clock
+
+upload: 
+	./arduino-cli upload \
+		-p $(PORT) \
+		--fqbn $(BOARD_FQBN) \
+		clock
---
+++ clock/clock.ino
@@ -0,0 +1,164 @@
+// LED matrix driver for max7219
+
+// NTP shamelessly ripped from https://tttapa.github.io/ESP8266/Chap15%20-%20NTP.html
+#include <SPI.h>
+#include "LEDPanel.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiUdp.h>
+#include <Ticker.h>
+Ticker renderTicker;
+
+
+
+// clk  D5
+// CS   D3
+// MOSI D7
+
+
+LEDPanel panel = LEDPanel(11, 13, 0, 4);
+int renderCount = 0;
+void renderPanel()
+{
+  if(renderCount == 0)
+  {
+    panel.scrollRender(0);
+  }
+  else if(renderCount > 15)
+  {
+    panel.scrollRender(-1);
+  }
+  if((++renderCount)>= panel.getScrollWindowSize())
+  {
+    renderCount = 0;
+  }
+}
+
+
+
+ESP8266WiFiMulti wifiMulti;      // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+WiFiUDP UDP;                     // Create an instance of the WiFiUDP class to send and receive
+IPAddress timeServerIP;          // time.nist.gov NTP server address
+const char* NTPServerName = "time.nist.gov";
+const int NTP_PACKET_SIZE = 48;  // NTP time stamp is in the first 48 bytes of the message
+byte NTPBuffer[NTP_PACKET_SIZE]; // buffer to hold incoming and outgoing packets
+int rejectCount = 0;
+
+void setup()
+{
+  panel.disable();
+  panel.setLEDIntensity(0);
+  Serial.begin(115200);
+  panel.enable();
+  panel.writeString("ABC",true);
+  renderTicker.attach(0.1, renderPanel);
+  Serial.println("Connecting...");
+  wifiMulti.addAP("ASK4 Wireless", "");   // add Wi-Fi networks
+  Serial.println("Connected!");
+  while (wifiMulti.run() != WL_CONNECTED) {  // Wait for the Wi-Fi to connect
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println("\r\n");
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());             // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.print(WiFi.localIP());            // Send the IP address of the ESP8266 to the computer
+  Serial.println("Starting UDP");
+  UDP.begin(123);                          // Start listening for UDP messages on port 123
+  Serial.print("Local port:\t");
+  Serial.println(UDP.localPort());
+  if(!WiFi.hostByName(NTPServerName, timeServerIP)) { // Get the IP address of the NTP server
+    Serial.println("DNS lookup failed. Rebooting.");
+    Serial.flush();
+    ESP.reset();
+  }
+  Serial.print("Time server IP:\t");
+  Serial.println(timeServerIP);
+  
+  Serial.println("\r\nSending NTP request ...");
+  sendNTPpacket(timeServerIP);  
+}
+
+void loop() 
+{
+  uint32_t unixTime = getTime();
+  if( unixTime != 0)
+  {
+    Serial.println("Got response!");
+    rejectCount=0;
+//    int seconds = getSeconds(unixTime);
+    char tempBuffer[2];
+    sprintf(tempBuffer, "%02d",getMinutes(unixTime));
+    String minutes = String(tempBuffer);
+    String hours = String(getHours(unixTime));
+    String output = "";
+    output.concat(hours);
+    output.concat(":");
+    output.concat(minutes);
+    panel.writeString(output,true);
+    delay(1000*30); // delay 30 seconds
+    sendNTPpacket(timeServerIP);  
+  }
+  else
+  {
+    Serial.println("Got nothing");
+    rejectCount++;
+    if(rejectCount > 3600) // if cannot talk to server for 1 hour, esp reboots
+    {
+      ESP.reset();
+    }
+    // send another request after 1 second
+    sendNTPpacket(timeServerIP);  
+    delay(1000);
+  }
+
+  // try to get time
+//  uint32_t unixTime = getTime();
+//  if( unixTime != 0) {
+//    rejectCount = 0;
+//  } else {
+//    rejectCount++;
+//  }
+  
+
+  
+}
+
+
+uint32_t getTime() {
+  if (UDP.parsePacket() == 0) { // If there's no response (yet)
+    return 0;
+  }
+  UDP.read(NTPBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+  // Combine the 4 timestamp bytes into one 32-bit number
+  uint32_t NTPTime = (NTPBuffer[40] << 24) | (NTPBuffer[41] << 16) | (NTPBuffer[42] << 8) | NTPBuffer[43];
+  // Convert NTP time to a UNIX timestamp:
+  // Unix time starts on Jan 1 1970. That's 2208988800 seconds in NTP time:
+  const uint32_t seventyYears = 2208988800UL;
+  // subtract seventy years:
+  uint32_t UNIXTime = NTPTime - seventyYears;
+  return UNIXTime;
+}
+
+void sendNTPpacket(IPAddress& address) {
+  memset(NTPBuffer, 0, NTP_PACKET_SIZE);  // set all bytes in the buffer to 0
+  // Initialize values needed to form NTP request
+  NTPBuffer[0] = 0b11100011;   // LI, Version, Mode
+  // send a packet requesting a timestamp:
+  UDP.beginPacket(address, 123); // NTP requests are to port 123
+  UDP.write(NTPBuffer, NTP_PACKET_SIZE);
+  UDP.endPacket();
+}
+
+inline int getSeconds(uint32_t UNIXTime) {
+  return UNIXTime % 60;
+}
+
+inline int getMinutes(uint32_t UNIXTime) {
+  return UNIXTime / 60 % 60;
+}
+
+inline int getHours(uint32_t UNIXTime) {
+  return UNIXTime / 3600 % 24;
+}
